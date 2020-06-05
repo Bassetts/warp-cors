a6a930fc9b22365c82f9a5828c1f768612ca1f16
Jason Liquorish <jason@liquori.sh>
2020-06-05 18:02:33 +0100

    Handle redirects
    Adds reqwest as Hyper client doesn't handle redirects

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
-use hyper_rustls::HttpsConnector;
 use log::trace;
+use reqwest::Client;
 use warp::http::header::SET_COOKIE;
-use warp::hyper::client::{Client, HttpConnector};
+use warp::http::request::Parts;
 use warp::hyper::{self, Body, Response};
 
 use crate::error;
@@ -10,22 +10,52 @@ pub type Request = hyper::Request<Body>;
 
 #[derive(Clone)]
 pub(crate) struct HttpsClient {
-    client: Client<HttpsConnector<HttpConnector>>,
+    client: Client,
 }
 
 impl HttpsClient {
     pub(crate) fn new() -> Self {
-        let https = HttpsConnector::new();
-        let client = Client::builder().build(https);
+        let client = Client::new();
         HttpsClient { client }
     }
 
     pub(crate) async fn request(&self, request: Request) -> Result<Response<Body>, error::Error> {
+        let (parts, body) = request.into_parts();
+
+        let Parts {
+            method,
+            uri,
+            headers,
+            ..
+        } = parts;
+
+        let url = reqwest::Url::parse(&uri.to_string())?;
+        let body = reqwest::Body::wrap_stream(body);
+        let request = self
+            .client
+            .request(method, url)
+            .headers(headers)
+            .body(body)
+            .build()?;
+
         trace!("Sending request");
-        let mut response = self.client.request(request).await?;
+        let response = self.client.execute(request).await?;
         trace!("Got response");
 
-        response.headers_mut().remove(SET_COOKIE);
+        // Have to assign SET_COOKIE to a local variable here to avoid
+        // https://github.com/rust-lang/rust-clippy/issues/3825
+        let set_cookie = SET_COOKIE;
+        let response_headers = response.headers().iter().filter(|(k, _)| k != &set_cookie);
+
+        let mut builder = Response::builder();
+
+        for (key, value) in response_headers {
+            builder = builder.header(key, value);
+        }
+
+        let response = builder
+            .status(response.status())
+            .body(Body::wrap_stream(response.bytes_stream()))?;
 
         Ok(response)
     }
